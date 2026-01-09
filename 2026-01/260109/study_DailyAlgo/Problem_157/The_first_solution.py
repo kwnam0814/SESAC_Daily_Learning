a78cfe3670e9a7594cc6a633b9de5ae106098a71
kwnam0814 <kwnam0814@naver.com>
2026-01-09 15:38:15 +0900

    157_Python_first_solution

---
+++ 2026-01/260109/study_DailyAlgo/Problem_157/The_first_solution.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+
+    while n != 1:
+        answer += 1
+        if n % 2 == 0:
+            n = n / 2
+        else: # 1보다 큰 홀수인데, 1은 여기 올 수 없음
+            n = n * 3 + 1
+
+    return answer
+
+print(solution(6))
+print(solution(1))
+print(solution(11))
