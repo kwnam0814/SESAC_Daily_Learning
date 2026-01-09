8b9b3662f159aabf885e7ac58c75b4e7a94d2852
kwnam0814 <kwnam0814@naver.com>
2026-01-09 15:49:02 +0900

    158_Python_first_solution

---
+++ 2026-01/260109/study_DailyAlgo/Problem_158/The_first_solution.py
@@ -0,0 +1,14 @@
+def solution(n):
+    # answer = True
+
+    str_n = str(n)
+
+    for i in range(len(str_n)):
+        if str_n[i] == str_n[len(str_n) - 1 - i]:
+            return True
+        else:
+            return False
+        
+print(solution(131))
+print(solution(12345))
+print(solution(7))
