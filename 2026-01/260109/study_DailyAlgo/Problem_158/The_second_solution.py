89e8dae32ae239271e2be5fbff5e53fd5eede030
kwnam0814 <kwnam0814@naver.com>
2026-01-09 15:50:24 +0900

    158_Python_second_solution

---
+++ 2026-01/260109/study_DailyAlgo/Problem_158/The_second_solution.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = True
+
+    str_n = str(n)
+
+    for i in range(len(str_n)):
+        if str_n[i] != str_n[len(str_n) - 1 - i]:
+            answer = False
+            break
+            
+    return answer
+
+print(solution(131))
+print(solution(12345))
+print(solution(7))
