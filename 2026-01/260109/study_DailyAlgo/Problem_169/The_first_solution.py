94e4fec6d034b001a7a3410f76807269da60c9cb
kwnam0814 <kwnam0814@naver.com>
2026-01-09 17:00:31 +0900

    169_Python_first_solution

---
+++ 2026-01/260109/study_DailyAlgo/Problem_169/The_first_solution.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0 # 손뼉치는 횟수
+
+    # str_n = str(n)
+
+    for i in range(1, n+1):
+        str_i = str(i)
+
+        if ("3" in str_i) or ("6" in str_i) or ("9" in str_i):
+            answer = answer + (str_i.count("3")) + (str_i.count("6")) + (str_i.count("9"))
+
+    return answer
+
+print(solution(10))
+print(solution(26))
+print(solution(40))
